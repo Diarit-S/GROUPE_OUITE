6ba2ca72f2890b236962a4b7300cdbf65067153e
ETombuyses <e.tombuyses@gmail.com>
2019-06-27 22:33:48 +0200

    delete transition tag

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <transition name="fade">
-      <router-view></router-view>
-    </transition>
+    <router-view></router-view>
     <footer> 
       <ul class="links">
         <!-- <li>
